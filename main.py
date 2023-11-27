00a8c18ef159883f58fc2b260d24e5d79b13d93d
jszafran <jszafran.pv@gmail.com>
2023-11-27 11:32:59 +0100

    Fix base vs quote currency & change endpoint params.

--- main.py
+++ main.py
@@ -15,18 +15,22 @@ app = FastAPI()
 
 RATES_DATA = {
     "PLN": {
-        "USD": 4.3188,
-        "EUR": 4.5892,
         "PLN": 1.0,
-    }
+    },
+    "EUR": {
+        "PLN": 4.5892,
+    },
+    "USD": {
+        "PLN": 4.3188,
+    },
 }
 
 
-class BaseCurrency(str, Enum):
+class QuoteCurrency(str, Enum):
     PLN = "PLN"
 
 
-class Currency(str, Enum):
+class BaseCurrency(str, Enum):
     EUR = "EUR"
     PLN = "PLN"
     USD = "USD"
@@ -35,29 +39,29 @@ class Currency(str, Enum):
 class HistoricalRateResponse(BaseModel):
     for_date: date
     base_currency: BaseCurrency
-    currency: Currency
+    quote_currency: QuoteCurrency
     rate: Optional[float] = None
 
 
 @app.get("/api/v1/historical_rates")
 async def historical_rates(
-    for_date: date, base_currency: BaseCurrency, currency: Currency
+    for_date: date, base_currency: BaseCurrency, quote_currency: QuoteCurrency
 ):
     time.sleep(random.randrange(200, 300) / 1000)
-    if for_date != date(2023, 9, 25) and currency != base_currency:
+    if for_date != date(2023, 9, 25) and quote_currency != base_currency:
         return HistoricalRateResponse(
             for_date=for_date,
             base_currency=base_currency,
-            currency=currency,
+            quote_currency=quote_currency,
             rate=None,
         )
 
-    rate = RATES_DATA.get(base_currency).get(currency)
+    rate = RATES_DATA.get(base_currency).get(quote_currency)
 
     return HistoricalRateResponse(
         for_date=for_date,
         base_currency=base_currency,
-        currency=currency,
+        quote_currency=quote_currency,
         rate=rate,
     )
 
